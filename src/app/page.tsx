dea2ebeb1f549a02850d0bc182865c590caed50c
ahmedanwarabdulaziz <ahmedanwarabdulaziz@gmail.com>
2026-03-27 20:52:52 +0200

    Fix Vercel route collision: removed duplicated root page

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function Home() {
-  // Redirection to the main corporate systems portal.
-  // The middleware will automatically intercept this and redirect to /login if the user is not authenticated.
-  redirect('/company')
-}
